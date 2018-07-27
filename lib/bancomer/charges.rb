bbcdc058a7919541fc5147fafd6982073b86f213
manu <manuel.guevara@openpay.mx>
2018-07-27 10:21:37 -0500

    fix tokens minor errors, add customer level to charges

--- lib/bancomer/charges.rb
+++ lib/bancomer/charges.rb
@@ -2,31 +2,61 @@ require 'bancomer_resource'
 
 class Charges < BancomerResource
 
-  def refund(transaction_id, description)
-    post(description, transaction_id + '/refund')
+  def refund(transaction_id, description, customer_id=nil)
+    if customer_id
+      customers=@api_hook.create(:customers)
+      customers.refund_charge(customer_id, transaction_id, description)
+    else
+      post(description, transaction_id+'/refund')
+    end
   end
 
-  def capture(transaction_id)
-    post('', transaction_id + '/capture')
+  def capture(transaction_id, customer_id=nil)
+    if customer_id
+      customers=@api_hook.create(:customers)
+      customers.capture_charge(customer_id, transaction_id)
+    else
+      post('', transaction_id+'/capture')
+    end
   end
 
   def confirm_capture(options)
+
+    customer_id = options.fetch(:customer_id) { '' }
     transaction_id = options.fetch(:transaction_id)
     amount = options.fetch(:amount)
 
-    raise BancomerException if amount.nil? || transaction_id.nil?
+    if amount.nil? or transaction_id.nil?
+      raise OpenpayException.new
+    end
 
     amount_hash = { amount: amount }
-    post(amount_hash, transaction_id + '/capture')
+
+    unless customer_id.empty?
+      customers=@api_hook.create(:customers)
+      customers.confirm_customer_capture(customer_id, transaction_id, amount_hash)
+    else
+      post(amount_hash, transaction_id+'/capture')
+    end
 
   end
 
-  def get(charge='')
-    super charge
+  def get(charge='', customer_id=nil)
+    if customer_id
+      customers=@api_hook.create(:customers)
+      customers.get_charge(customer_id, charge)
+    else
+      super charge
+    end
   end
 
-  def create(charge)
-    super charge
+  def create(charge, customer_id=nil)
+    if customer_id
+      customers=@api_hook.create(:customers)
+      customers.create_charge(customer_id, charge)
+    else
+      super charge
+    end
   end
 
 end
